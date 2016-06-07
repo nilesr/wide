ba6158164084ab629763202e11afd1477cefd0b0
Niles Rogoff <nilesrogoff@gmail.com>
2016-06-07 00:55:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Tool for splitting a multi monitor wallpaper into individual images
+
+## Dependancies
+- [Pillow]
+- xrandr
+
+## Usage
+
+`python3 wide.py wide_base.png`
+
+If you are using xfce4, this will automatically set the wallpaper for each screen to the newly generated image. If you aren't, it will at least generate the images. Each monitor's image is listed as its monitor index from left to right. It scrapes the output of `xrandr -q` to get the current display configuration.
+
+## Example
+
+My xrandr setup at home looks like this
+![](http://i.imgur.com/jyr1vtr.png)
+
+The base image I used was this
+![](http://i.imgur.com/Q5NjyQc.jpg)
+
+After running `python3 wide.py wide_base.png` it created three images:
+![](http://i.imgur.com/Xydzqpw.jpg)
+![](http://i.imgur.com/Mlnog8c.jpg)
+![](http://i.imgur.com/tUGvU8v.jpg)
+
+It then set my wallpaper for each monitor to the appropriate image. A screenshot looked like this
+![](http://i.imgur.com/J8LB4Pt.jpg)
+
+## Known problems
+
+Currently the code is written assuming that the aspect ratio of your image is greater than the aspect ratio of your monitor setup, i.e. there will be some amount of unused image on the right. If there is not it will probably fail. Just crop the image then run the script.
---
+++ wide.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+import subprocess, PIL.Image, sys, os
+res = []
+for line in subprocess.check_output(["xrandr","-q"]).decode("utf-8").split("\n"):
+    if " connected" in line:
+        l = line.split()
+        if l[2] == "primary":
+            res.append(l[3])
+        else:
+            res.append(l[2])
+# Debug
+# res = ["1440x900+0+0", "1366x768+1400+376", "1440x900+2806+0"]
+res = [x.replace("+","x").split("x") for x in res]
+res = [[int(x) for x in f] for f in res]
+res.sort(key = lambda x: x[2]) # order monitors from left to right by sorting them by left offset
+image = PIL.Image.open(sys.argv[1])
+total_screen_width = max(x[0] + x[2] for x in res)
+total_screen_height = max(x[1] + x[3] for x in res)
+ires = []
+for r in res:
+    height_percentage = float(r[1])/total_screen_height
+    height = round(height_percentage * image.size[1])
+    width = round(height * (float(r[0])/r[1]))
+    top_offset_percent = float(r[3]) / total_screen_height
+    top_offset = round(top_offset_percent * image.size[1])
+    left_offset = sum(x[0] for x in ires)
+    ires.append([width, height, left_offset, top_offset])
+x = 0
+left_offset = 0
+if len(sys.argv) > 2:
+    left_offset = sys.argv[2]
+for h in ires:
+    print(h)
+    #left upper right lower
+    subimage = image.crop((h[2] + left_offset, h[3], h[2]+h[0] + left_offset, h[3]+h[1]))
+    subimage.save(str(x) + ".png")
+    print(subimage.size)
+    x += 1
+for x in range(len(ires)):
+    subprocess.call(["xfconf-query", "-c", "xfce4-desktop", "-p", "/backdrop/screen0/monitor"+str(x)+"/workspace0/last-image", "-s", os.getcwd() + "/" + str(x) + ".png"])
